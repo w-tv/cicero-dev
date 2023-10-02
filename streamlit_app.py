9fcd584adea4ca2ec6339ffa6c7052a45c8d4d72
w-tv <wcarpenter@targetedvictory.com>
2023-10-02 12:51:41 -0700

    stop trying to do things the 'right way', and just get er done instead

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,9 @@ import pandas as pd
 import requests
 import json
 
-st.title('![💬](/logo.png) Cicero') #this displays, but I don't think you can resize images in github markdown, which is what st uses. I tried using literal html in this string and it just displayed that as text.
-# <img src="favicon.png" style="display:inline-block; height:100%; width:auto;"> #this is how I would display it to resize correctly, I guess (untested), but st won't let me use html.
+#Also, it could not find the image in my project using relative paths?? # st.title('![💬](logo.png) Cicero') #this displays, but I don't think you can resize images in github markdown, which is what st uses. I tried using literal html in this string and it just displayed that as text.
+bespoke_title_element = '<h1><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png" alt="💬" style="display:inline-block; height:100%; width:auto;"> Cicero</h1>'
+st.markdown(bespoke_title_element, unsafe_allow_html=True)
 st.caption("It's pronounced ‘Kickero’")
 model_uri = 'https://dbc-ca8d208b-aaa9.cloud.databricks.com/serving-endpoints/pythia/invocations'
 databricks_api_token = 'dapi360d025c9e135c809de05abbf3196a06'
