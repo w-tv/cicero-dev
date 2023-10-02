7548cf7f7bba725da9fa5b411dbd029cc99b11dc
w-tv <wcarpenter@targetedvictory.com>
2023-10-02 12:06:54 -0700

    misc 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ import pandas as pd
 import requests
 import json
 
-st.title("![logo](https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png) Cicero")
+st.title('<title>💬Cicero</title><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png"> Cicero')
+# ![logo](https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png) #this displays, but I don't think you can resize in github markdown
 st.caption("It's pronounced ‘Kickero’")
 model_uri = 'https://dbc-ca8d208b-aaa9.cloud.databricks.com/serving-endpoints/pythia/invocations'
 databricks_api_token = 'dapi360d025c9e135c809de05abbf3196a06'
