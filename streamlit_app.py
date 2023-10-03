179b11f311a260445c3243965fd4b6bb6387e854
w-tv <wcarpenter@targetedvictory.com>
2023-10-03 09:52:09 -0700

    clean up code a little

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,7 @@ import pandas as pd
 import requests
 import json
 
-#Also, it could not find the image in my project using relative paths?? # st.title('![💬](logo.png) Cicero') #this displays, but I don't think you can resize images in github markdown, which is what st uses. I tried using literal html in this string and it just displayed that as text.
-bespoke_title_element = '<script type="text/javascript">document.title = "💬 Cicero";</script><h1><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png" alt="💬" style="display:inline-block; height:1em; width:auto;"> Cicero</h1>'
+bespoke_title_element = '<h1><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png" alt="💬" style="display:inline-block; height:1em; width:auto;"> Cicero</h1>'
 st.markdown(bespoke_title_element, unsafe_allow_html=True)
 st.caption("It's pronounced ‘Kickero’")
 model_uri = 'https://dbc-ca8d208b-aaa9.cloud.databricks.com/serving-endpoints/pythia/invocations'
@@ -38,7 +37,7 @@ def tokenize_and_send(prompt):
   tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-160m-deduped")
   text_ids = tokenizer.encode(prompt, return_tensors = 'pt')
   output = str(score_model(model_uri, databricks_api_token, text_ids))
-  st.write(output[16:-1]) #I guess we don't like the first 16 characters of this for some reason.
+  st.write(output[16:-1]) #I guess we don't like the first 16, and last 1, characters of this for some reason. Note: I have been informed that the first 16 and last 1 characters are, like "Response: { " and "}" or something.
 
 tone_indictators_sorted = ["Urgency", "Agency", "Exclusivity"]
 
@@ -53,8 +52,8 @@ def list_to_bracketeds_string(l: list[str]) -> str:
   s = ""
   is_first_item = True
   for i in l:
-    s += " " if not is_first_item else ""
-    is_first_item = False #malarkey 
+    s += " " if not is_first_item else "" #malarkey to space only between bracketeds, not before or after
+    is_first_item = False
     s += ("["+i.strip().replace(" ", "_")+"]")
   return s
 
@@ -62,11 +61,9 @@ account = st.selectbox("Account", ["Tim Scott", "Steve Scalise"])
 ask_type = st.selectbox("Ask type", ["fundraising hard ask text", "fundraising medium ask text", "fundraising soft ask text", "list building text", "other text"])
 tone = st.multiselect("Tone", tone_indictators_sorted)
 topics = st.multiselect("Topics", ["GOP", "Control", "Dems", "Crime", "Military", "GovOverreach", "Religion"])
-additional_topics = [x for x in st.text_input("Additional topics (free text entry, separated by commas. Example: China, State of the Race, Match)").split(",") if x.strip()] #filter out empty strings on split, too.
+additional_topics = [x for x in st.text_input("Additional topics (free text entry, separated by commas. Example: China, State of the Race, Match)").split(",") if x.strip()] # The list comprehension is to filter out empty strings on split, because otherwise this fails to make a truly empty list in the default case, instead having a list with an empty string in, because split changes its behavior when you give it arguments. Anyway, this also filters out trailing comma edge-cases and such.
 generate_button = st.button("Generate a message based on the above by clicking this button!")
 
-# if chat_prompt := st.chat_input("Or, compose a full message here."): #removed as not useful to end-user
-#  tokenize_and_send(chat_prompt)
 #TODO: add bio dict and add the bio to context before prompt. In tokenize_and_send I guess. Maybe rename that to "fluff and send".
 if generate_button:
   button_prompt = "Write a "+ask_type+" for "+account+" about: "+list_to_bracketeds_string(topics+additional_topics or ["No_Hook"])+( "" if not tone else " emphasizing "+ list_to_bracketeds_string(sortedUAE(tone)) )
