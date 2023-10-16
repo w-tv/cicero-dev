6042407f09461dde69c799345af3e562e31ff52f
achangtv <104849382+achangtv@users.noreply.github.com>
2023-10-16 13:11:59 -0400

    remove prints
    removed prints because they weren't printing, could only see the exception, maybe they'd work if there wasn't an error
    
    removed the "index": [0], part of data_json, hopefully this works

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,8 @@ def send(model_uri, databricks_token, data):
     "Content-Type": "application/json",
   }
   ds_dict = {'dataframe_split': data.to_dict(orient='split')} if isinstance(data, pd.DataFrame) else create_tf_serving_json(data)
-  print(ds_dict, type(ds_dict))
   data_json = json.dumps(ds_dict, allow_nan=True)
-  print(data_json, type(data_json))
+  data_json = data_json[0:21] + data_json[35:]
   response = requests.request(method='POST', headers=headers, url=model_uri, json=data_json)
   if response.status_code == 504:
     return send(model_uri, databricks_token, data) #we recursively call this until the machine wakes up.
@@ -51,7 +50,6 @@ def promptify_and_send(prompt):
                   "max_new_tokens" : [int(target_charcount_max) // 4],
                   "min_new_tokens" : [int(target_charcount_min) // 4]}
   df_prompt = pd.DataFrame(dict_prompt)
-  print(df_prompt)
   output = str(send(model_uri, databricks_api_token, df_prompt))
   pure_output = output[16:-1]
   st.write(pure_output)
