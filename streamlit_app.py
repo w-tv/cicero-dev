ab29d516d0bd921b2ad6e4e2236765d8fa11f720
w-tv <wcarpenter@targetedvictory.com>
2023-10-16 13:22:02 -0700

    (try again) hardcode for new endpoint

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ data_json = """{
       ]
     ]
   }
-}"""
+}""" #TODO: Remove hard-coding when can
 
 def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
@@ -69,10 +69,8 @@ def send(model_uri, databricks_token, data):
     "Content-Type": "application/json",
   }
   ds_dict = {'dataframe_split': data.to_dict(orient='split')} if isinstance(data, pd.DataFrame) else create_tf_serving_json(data)
-  data_json = json.dumps(ds_dict, allow_nan=True)
+  #data_json = json.dumps(ds_dict, allow_nan=True)
   #data_json = data_json[0:21] + data_json[35:]
-  st.write(data_json)
-  data_json = '{"dataframe_split": {"index": [0], "columns": ["prompt", "temperature", "max_new_tokens", "min_new_tokens"], "data": [["<|startoftext|> Write a fundraising hard ask text for Andy Barr about: [No_Hook] <|body|>", 0.7, 20, 40]]}}' #TODO: Remove hard-coding when can
   st.write(data_json)
   st.write(headers)
   st.write(model_uri)
