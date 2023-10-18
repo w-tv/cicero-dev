3ba9ab6b6ac6625b84ef6822551673445aec19cf
w-tv <wcarpenter@targetedvictory.com>
2023-10-18 16:05:50 -0700

    reset only works for 'target number of characters, minimum' right now, because it's only set up for that, but it finally works woohoo!

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,41 +8,22 @@ import subprocess
 import requests
 import json
 import os
+from streamlit.elements.utils import _shown_default_value_warning
+_shown_default_value_warning = True #ahaha! see https://discuss.streamlit.io/t/why-do-default-values-cause-a-session-state-warning/15485/8 for why we do this. We could restructure the entire program, but this trick is easier.
 
-#Make default state, and other presets, so we can manage presets and resets.
-st.session_state.presets = {
-  "default": {
-    "top_k": 50,
-  },
-}
-st.session_state.current_preset = st.session_state.presets["default"]
-
-with st.sidebar:
-  st.header("Options for the model:")
-  st.caption("These controls can be optionally adjusted to influence the way that the model generates text, such as the length and variety of text the model will attempt to make the text display.")
-  temperature : float = st.slider("Textual variety (‘temperature’):", min_value=0.0, max_value=1.0, value=0.7) #temperature: slider between 0 and 1, defaults to 0.7, float
-  #character count max, min: int, cannot be negative or 0, I guess it has to be above about 40. floor divide by 4 to get token count to pass to model:
-  target_charcount_min = st.number_input("Target number of characters, minimum:", min_value=40, value=160, format='%d', step=1)
-  target_charcount_max = st.number_input("Target number of characters, maximum:", min_value=40, value=80, format='%d', step=1)
-  with st.expander("Advanced options (not yet hooked up)"):
-    #TODO: not hooked up yet
-    num_beams = st.number_input("num_beams:", min_value=1, value=1, format='%d', step=1, help="Number of beams for beam search. 1 means no beam search. Beam search is a particular strategy for generating text that the model can elect to use or not use. It can use more or fewer beams in the beam search, as well. More beams basically means it considers more candidate possibilities.")
-    a = st.empty()
-    top_k = a.number_input("top_k:", min_value=1, value=st.session_state.current_preset["top_k"], format='%d', step=1, help="The number of highest probability vocabulary tokens to keep for top-k-filtering. In other words: how many likely words the model will consider.")
-    top_p = st.number_input("top_p:", min_value=0.0, value=1.0, format='%f', help="A decimal number, not merely an integer. If set to < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation. In other words: if you reduce this number below 1, the model will consider fewer possibilities.")
-    repetition_penalty = st.number_input("repetition_penalty:", min_value=1.0, value=1.5, format='%f', help="A decimal number, not merely an integer. The parameter for repetition penalty. 1.0 means no penalty. In other words: if you increase this parameter, the model will be less likely to repeat itself.")
-    no_repeat_ngram_size = st.number_input("no_repeat_ngram_size:", min_value=0, value=4, format='%d', step=1, help="If set to > 0, all ngrams (essentially, continuous sequences of words or word-parts) of that size can only occur once. In other words: if you set this parameter to a number greater than 0, any string of words can only occur in the output at most that many times.")
-    num_return_sequences = st.number_input("num_return_sequences:", min_value=1, value=4, format='%d', step=1, help="The number of independently computed returned sequences for each element in the batch. In other words: how many responses you want the model to generate.")
-    early_stopping = st.checkbox("early_stopping", value=False, help="Controls the stopping condition for beam-based methods, like beam-search. It accepts the following values: True, where the generation stops as soon as there are num_beams complete candidates; False, where an heuristic is applied and the generation stops when is it very unlikely to find better candidates; \"never\", where the beam search procedure only stops when there cannot be better candidates (canonical beam search algorithm). In other words: if the model is using beam search (see num_beams, above), then if this box is checked the model will spend less time trying to improve its beams after it generates them. If num_beams = 1, this checkbox does nothing either way. There is no way to select \"never\" using this checkbox, as that setting is just a waste of time.")
-    do_sample = st.checkbox("do_sample", value=True, help="Whether or not to use sampling ; use greedy decoding otherwise. These are two different strategies the model can use to generate text. Greedy is probably much worse, and you should probably always keep this box checked.")
-    output_scores = st.checkbox("output_scores", value=False, help="Whether or not to return the prediction scores. See scores under returned tensors for more details. In other words: This will not only give you back responses, like normal, it will also tell you how likely the model thinks the response is. Usually useless, and there's probably no need to check this box.")
-  st.header("History of replies (higher = more recent):")
-
-bespoke_title_element = '<h1><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png" alt="💬" style="display:inline-block; height:1em; width:auto;"> CICERO</h1>'
-st.markdown(bespoke_title_element, unsafe_allow_html=True)
 bios : dict[str, str] = dict(pd.read_csv("Candidate_Bios.csv", index_col="ID").to_dict('split')['data'])
 model_uri = st.secrets['model_uri']
 databricks_api_token = st.secrets['databricks_api_token']
+
+#Make default state, and other presets, so we can manage presets and resets.
+# Ah, finally, I've figured out how you're actually supposed to do it: https://docs.streamlit.io/library/advanced-features/button-behavior-and-examples#option-1-use-a-key-for-the-button-and-put-the-logic-before-the-widget
+presets = {
+  "default": {
+    "top_k": 50,
+    "target_charcount_min": 160,
+  },
+}
+current_preset = presets["default"]
 
 def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
@@ -107,20 +88,52 @@ def list_to_bracketeds_string(l: list[str]) -> str:
     s += ("["+i.strip().replace(" ", "_")+"]")
   return s
 
-account = st.selectbox("Account", list(bios)) #in case you're confused: list of a dict creates a list of the keys of the dict
-ask_type = st.selectbox("Ask Type", ["Fundraising Hard Ask", "Fundraising Medium Ask", "Fundraising Soft Ask", "List Building"], key="ask_type")
-tone = st.multiselect("Tone", tone_indictators_sorted)
-topics = st.multiselect("Topics", ["Bio", "GOP", "Control", "Dems", "Crime", "Military", "GovOverreach", "Religion"])
-additional_topics = [x for x in st.text_input("Additional Topics (Example: Biden, Survey, Deadline)").split(",") if x.strip()] # The list comprehension is to filter out empty strings on split, because otherwise this fails to make a truly empty list in the default case, instead having a list with an empty string in, because split changes its behavior when you give it arguments. Anyway, this also filters out trailing comma edge-cases and such.
-generate_button = st.button("Submit")
-if st.button("Reset all fields"): st.rerun() #COULD: implement later, along with presets. The user currently just has to f5
+ui_container = st.container()
+
+def draw_ui(preset: dict):
+  with ui_container:
+    with st.sidebar:
+      st.header("Options for the model:")
+      st.caption("These controls can be optionally adjusted to influence the way that the model generates text, such as the length and variety of text the model will attempt to make the text display.")
+      temperature : float = st.slider("Textual variety (‘temperature’):", min_value=0.0, max_value=1.0, value=0.7) #temperature: slider between 0 and 1, defaults to 0.7, float
+      #character count max, min: int, cannot be negative or 0, I guess it has to be above about 40. floor divide by 4 to get token count to pass to model:
+      target_charcount_min = st.number_input("Target number of characters, minimum:", min_value=40, value=preset["target_charcount_min"], format='%d', step=1, key="target_charcount_min")
+      target_charcount_max = st.number_input("Target number of characters, maximum:", min_value=40, value=80, format='%d', step=1)
+      with st.expander("Advanced options (not yet hooked up)"):
+        #TODO: not hooked up yet
+        num_beams = st.number_input("num_beams:", min_value=1, value=1, format='%d', step=1, help="Number of beams for beam search. 1 means no beam search. Beam search is a particular strategy for generating text that the model can elect to use or not use. It can use more or fewer beams in the beam search, as well. More beams basically means it considers more candidate possibilities.")
+        a = st.empty()
+        top_k = a.number_input("top_k:", min_value=1, value=preset["top_k"], format='%d', step=1, help="The number of highest probability vocabulary tokens to keep for top-k-filtering. In other words: how many likely words the model will consider.")
+        top_p = st.number_input("top_p:", min_value=0.0, value=1.0, format='%f', help="A decimal number, not merely an integer. If set to < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation. In other words: if you reduce this number below 1, the model will consider fewer possibilities.")
+        repetition_penalty = st.number_input("repetition_penalty:", min_value=1.0, value=1.5, format='%f', help="A decimal number, not merely an integer. The parameter for repetition penalty. 1.0 means no penalty. In other words: if you increase this parameter, the model will be less likely to repeat itself.")
+        no_repeat_ngram_size = st.number_input("no_repeat_ngram_size:", min_value=0, value=4, format='%d', step=1, help="If set to > 0, all ngrams (essentially, continuous sequences of words or word-parts) of that size can only occur once. In other words: if you set this parameter to a number greater than 0, any string of words can only occur in the output at most that many times.")
+        num_return_sequences = st.number_input("num_return_sequences:", min_value=1, value=4, format='%d', step=1, help="The number of independently computed returned sequences for each element in the batch. In other words: how many responses you want the model to generate.")
+        early_stopping = st.checkbox("early_stopping", value=False, help="Controls the stopping condition for beam-based methods, like beam-search. It accepts the following values: True, where the generation stops as soon as there are num_beams complete candidates; False, where an heuristic is applied and the generation stops when is it very unlikely to find better candidates; \"never\", where the beam search procedure only stops when there cannot be better candidates (canonical beam search algorithm). In other words: if the model is using beam search (see num_beams, above), then if this box is checked the model will spend less time trying to improve its beams after it generates them. If num_beams = 1, this checkbox does nothing either way. There is no way to select \"never\" using this checkbox, as that setting is just a waste of time.")
+        do_sample = st.checkbox("do_sample", value=True, help="Whether or not to use sampling ; use greedy decoding otherwise. These are two different strategies the model can use to generate text. Greedy is probably much worse, and you should probably always keep this box checked.")
+        output_scores = st.checkbox("output_scores", value=False, help="Whether or not to return the prediction scores. See scores under returned tensors for more details. In other words: This will not only give you back responses, like normal, it will also tell you how likely the model thinks the response is. Usually useless, and there's probably no need to check this box.")
+      st.header("History of replies (higher = more recent):")
+
+    bespoke_title_element = '<h1><img src="https://targetedvictory.com/wp-content/uploads/2019/07/favicon.png" alt="💬" style="display:inline-block; height:1em; width:auto;"> CICERO</h1>'
+    st.markdown(bespoke_title_element, unsafe_allow_html=True)
+
+    account = st.selectbox("Account", list(bios)) #in case you're confused: list of a dict creates a list of the keys of the dict
+    ask_type = st.selectbox("Ask Type", ["Fundraising Hard Ask", "Fundraising Medium Ask", "Fundraising Soft Ask", "List Building"], key="ask_type")
+    tone = st.multiselect("Tone", tone_indictators_sorted)
+    topics = st.multiselect("Topics", ["Bio", "GOP", "Control", "Dems", "Crime", "Military", "GovOverreach", "Religion"])
+    additional_topics = [x for x in st.text_input("Additional Topics (Example: Biden, Survey, Deadline)").split(",") if x.strip()] # The list comprehension is to filter out empty strings on split, because otherwise this fails to make a truly empty list in the default case, instead having a list with an empty string in, because split changes its behavior when you give it arguments. Anyway, this also filters out trailing comma edge-cases and such.
+    generate_button = st.button("Submit")
+    if generate_button:
+      button_prompt = ((bios[account]+"\n\n") if "Bio" in topics else "") +"Write a "+ask_type.lower()+" text for "+account+" about: "+list_to_bracketeds_string(topics+additional_topics or ["No_Hook"])+( "" if not tone else " emphasizing "+ list_to_bracketeds_string(sortedUAE(tone)) )
+      promptify_and_send(button_prompt)
 
 #TODOS: breaking news checkbox
 #     Add reset button to page to clear all parameters, reset to defaults #this is how: https://discuss.streamlit.io/t/change-the-state-of-an-item-from-code/1429/4 also https://docs.streamlit.io/library/api-reference/layout/st.container but I'm not sure that has a empty() method on it you can call... I guess vs code autocomplete would know.
 #    allow for creation of presets (does not need to last between sessions) (for now)
-
-if generate_button:
-  button_prompt = ((bios[account]+"\n\n") if "Bio" in topics else "") +"Write a "+ask_type.lower()+" text for "+account+" about: "+list_to_bracketeds_string(topics+additional_topics or ["No_Hook"])+( "" if not tone else " emphasizing "+ list_to_bracketeds_string(sortedUAE(tone)) )
-  promptify_and_send(button_prompt)
+#make main and sidebar forms instead?
+st.session_state
+def myfun(): st.session_state["target_charcount_min"] = 160
+st.button("Reset all fields", on_click=myfun)
+  #if st.session_state["target_charcount_min"]: ; st.rerun()
+draw_ui(current_preset) #COULD: implement later, along with presets. The user currently just has to f5
 
 # html('<!--<script>//you can include arbitrary html and javascript this way</script>-->')
