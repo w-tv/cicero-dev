bb7bfb866f45cceb6bbc334890da7dcf2dcd1296
w-tv <wcarpenter@targetedvictory.com>
2024-02-16 15:17:19 -0800

    use a list comprehension for UAE tone sorting
    chang forgive me

--- streamlit_app.py
+++ streamlit_app.py
@@ -260,12 +260,9 @@ def send(model_uri: str, databricks_token: str, data: pd.DataFrame) -> list[str]
 
 tone_indictators_sorted = ["Urgency", "Agency", "Exclusivity"]
 
-def sortedUAE(unsorted_tones: list[str]) -> list[str]:
+def sortedUAE(unsorted_tone_indicators: list[str]) -> list[str]:
   """For some reason (mnemonic?) the canonical ordering of these tone tags is Urgency Agency Exclusivity. They never appear in any other order, although they do appear in every subset. Anyway, this function implements that ordering, regardless of the order the user selected them."""
-  sorted_tones = []
-  for indicator in tone_indictators_sorted:
-    if indicator in unsorted_tones: sorted_tones.append(indicator)
-  return sorted_tones
+  return [indicator for indicator in tone_indictators_sorted if indicator in unsorted_tone_indicators]
 
 def list_to_bracketeds_string(l: list[str]) -> str:
   s = ""
