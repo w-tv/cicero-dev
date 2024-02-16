1218148a06364e68620ade482589addc7a2876ea
w-tv <wcarpenter@targetedvictory.com>
2024-02-16 15:13:42 -0800

    add a little bit of styling to hook_reporting

--- pages/hook_reporting.py
+++ pages/hook_reporting.py
@@ -1,2 +1,8 @@
 import streamlit as st
+
+st.set_page_config(
+  layout="wide",
+  page_title="Hook Reporting", #this is of questionable usefulness, as it just changes the browser tab display.
+  page_icon="🪝",
+)
 st.write("TODO: put hook reporting here.")
