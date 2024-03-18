5c085ca986ea43d12f9d66a2eff223f5d9082122
w-tv <wcarpenter@targetedvictory.com>
2024-03-18 15:05:40 -0400

    change the streamlit_app.py file, which (to be clear) worked perfectly, to a symlink to cicero.py that does the same thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,1 @@
-# This file is just a holdover from the fact that we deploy from a hard-coded file name streamlit_app.py, so to avoid changing that for the time being we have this file, which invokes cicero
-import cicero
-cicero.main()
+cicero.py
