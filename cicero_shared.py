9e4bf90c25224fa611396b3e86a0bb71b6f213b9
w-tv <wcarpenter@targetedvictory.com>
2024-03-29 21:54:10 -0400

    mess about with requirements, types

--- cicero_shared.py
+++ cicero_shared.py
@@ -1,10 +1,13 @@
 from databricks import sql # Spooky that this is not the same name as the pypi package databricks-sql-connector, but is the way to refer to the same thing.
 from databricks.sql.types import Row
 import streamlit as st
-from typing import Any
+from typing import Any, cast
 
 @st.cache_data()
 def sql_call(query: str, sql_params_dict:dict[str, Any]|None=None) -> list[Row]: #The returned values are usually str, but occasionally might be int (as when getting the count) or float or perhaps any of these https://docs.databricks.com/en/dev-tools/python-sql-connector.html#type-conversions
   with sql.connect(server_hostname=st.secrets["DATABRICKS_SERVER_HOSTNAME"], http_path=st.secrets["DATABRICKS_HTTP_PATH"], access_token=st.secrets["databricks_api_token"]) as connection: #These secrets should be in the root level of the .streamlit/secrets.toml
     with connection.cursor() as cursor:
-      return cursor.execute(query, sql_params_dict).fetchall()
+      return cast(
+        list[Row],
+        cursor.execute(query, sql_params_dict).fetchall()
+      ) #I tentatively conclude this cast (casts are evil because they break the integrity of the type system) is a MYPY-BUG-WORKAROUND, because you can get the rest of databricks-sql-connector to typecheck properly, it seems, except for this... but it might be a databricks-sql-connector-bug-workaround.
