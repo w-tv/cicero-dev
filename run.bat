6a4bda1d215386ddd118220dfd953d7de458e4a4
w-tv <wcarpenter@targetedvictory.com>
2024-04-22 10:44:51 -0400

    update run to actually use the venv, windows edition

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 uv --version || pip install uv --disable-pip-version-check
-uv pip install -r requirements.txt
-streamlit run cicero.py
+uv pip install -r requirements.txt || uv venv && uv pip install -r requirements.txt
+call .venv/Scripts/activate
+.venv/Scripts/python -m streamlit run cicero.py
+deactivate
