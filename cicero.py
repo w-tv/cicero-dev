ca410ccfdac53530bfb0ee9783fa66632243a535
w-tv <wcarpenter@targetedvictory.com>
2024-05-20 12:52:03 -0400

    install cryptography, as an error message requested

--- cicero.py
+++ cicero.py
@@ -82,11 +82,12 @@ def main() -> None:
         Streamlit version: {st.__version__}<br>
         Base url: {get_base_url()}
       """, unsafe_allow_html=True)
+      st.write("aud", st.secrets["aud"])
       st.caption("Web headers:")
       st.write(h := _get_websocket_headers())
       if h:
         st.write("X-Goog-Authenticated-User-Email", h.get("X-Goog-Authenticated-User-Email"))
-        st.write("X-Goog-IAP-JWT-Assertion", h.get("X-Goog-IAP-JWT-Assertion"))
+        st.write("X-Goog-Iap-Jwt-Assertion", h.get("X-Goog-Iap-Jwt-Assertion"))
       if st.button("Crash the program."):
         exit_error(27)
       st.button("disable developer mode", on_click=disable_developer_mode, help="Click this button to disable developer mode, allowing you to see and interact with the app as a basic user would. You can refresh the page in your browser to re-enable developer mode.") #this is a callback for streamlit ui update-flow reasons.
