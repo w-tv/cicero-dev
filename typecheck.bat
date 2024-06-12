53e631cbf7b57afcbe8e2ce1455ede54781413ed
w-tv <wcarpenter@targetedvictory.com>
2024-06-12 15:32:24 -0400

    misc slight change to retrigger a build that failed

--- typecheck.bat
+++ typecheck.bat
@@ -4,7 +4,7 @@ uv pip install mypy
 echo "-------- Mypy, non-interactively installing types..."
 uv run mypy --install-types --non-interactive
 echo "-------- Mypy typechecking, ignoring missing imports (I have no idea why so many of our imports are missing types, but I am not going to spend time fixing that, so we use --ignore-missing-imports):"
-uv run python -c "import shutil; shutil.copytree('stubs_for_external_projects', '.venv/Lib/site-packages', dirs_exist_ok=True) #allows us to use our own stubs"
+uv run python -c "import shutil; shutil.copytree('stubs_for_external_projects', '.venv/Lib/site-packages', dirs_exist_ok=True) # Allows us to use our own stubs"
 uv run python -c "import os; f = '.venv/Lib/site-packages/torch/py.typed'; os.path.isfile(f) and os.remove(f) #PYTORCH-BUG-WORKAROUND: temporary fix for https://github.com/pytorch/pytorch/issues/124897 which is already fixed upstream I guess."
 uv run mypy cicero.py --strict --ignore-missing-imports --pretty
 uv run python -c "input('Press enter to exit...')"
