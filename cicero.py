76f0169fb14e5588fc57d7b296b05dc84bfbda97
w-tv <wcarpenter@targetedvictory.com>
2024-06-19 02:01:38 -0400

    improve pageload performance by removing neeed to scale image using PIL at runtime

--- cicero.py
+++ cicero.py
@@ -44,7 +44,7 @@ def main() -> None:
 
   title_and_loading_columns = st.columns(2)
   with title_and_loading_columns[0]:
-    st.image(image="assets/CiceroLogo_Frontend_Cropped.png", caption="", width=400, use_column_width=False)
+    st.image(image="assets/CiceroLogo_Frontend_Cropped_400x107.png", use_column_width=False)
   with title_and_loading_columns[1]:
     loading_message = st.empty()
     loading_message.write("Loading CICERO.  This may take up to a minute...")
