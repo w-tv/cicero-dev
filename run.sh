941ea35b6635e81c352008258a3c9f00376bc9ca
w-tv <wcarpenter@targetedvictory.com>
2024-06-20 15:20:46 -0400

    small tooling fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-echo if you dont use wsl, try (renaming the file extension, then) running run.sh directly as a bat script, it will work.
+echo "if you dont use wsl, try (renaming the file extension, then) running run.sh directly as a bat script, it will work."
 uv --version || pip install --disable-pip-version-check --break-system-packages uv
 uv pip install -r requirements.txt || uv venv && uv pip install -r requirements.txt
 uv run streamlit run cicero.py
