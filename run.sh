be9d109d9e21cba8ff9499266ed5b483aacf6481
w-tv <wcarpenter@targetedvictory.com>
2024-06-20 03:42:30 -0400

    add note into run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-uv --version || pip install uv --disable-pip-version-check --break-system-packages
+echo if you dont use wsl, try (renaming the file extension, then) running run.sh directly as a bat script, it will work.
+uv --version || pip install --disable-pip-version-check --break-system-packages uv
 uv pip install -r requirements.txt || uv venv && uv pip install -r requirements.txt
 uv run streamlit run cicero.py
 uv run python -c "input('Press enter to exit...')"
