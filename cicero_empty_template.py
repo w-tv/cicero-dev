ed0d84719052755f1038c7960bee2e6dc3f738fe
w-tv <wcarpenter@targetedvictory.com>
2024-07-04 09:59:25 -0400

    {+#!/usr/bin/env -S streamlit run+} {+""" This is a mostly-empty template for a tab/subpage of Cicero. It can also be run standalone using streamlit."""+} {+import streamlit as st+} {+from cicero_shared import sql_call+} {+def main() -> None:+} {+  pass+} {+if __name__ == "__main__": main()+} A  cicero_empty_template.py

---
+++ cicero_empty_template.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S streamlit run
+""" This is a mostly-empty template for a tab/subpage of Cicero. It can also be run standalone using streamlit."""
+import streamlit as st
+from cicero_shared import sql_call
+
+def main() -> None:
+  pass
+if __name__ == "__main__": main()
