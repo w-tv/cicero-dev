7e7a2f80b0d2189078d2b19ad6115bd0ecf7f7fa
w-tv <wcarpenter@targetedvictory.com>
2024-07-04 10:04:54 -0400

    add performance feature-disabling to the empty template

--- cicero_empty_template.py
+++ cicero_empty_template.py
@@ -5,4 +5,12 @@ from cicero_shared import sql_call
 
 def main() -> None:
   pass
+  if st.session_state.get("REPLACEME_enabled"):
+    pass
+  else:
+    st.caption("To improve performance for the rest of the app, this functionality is disabled in a session by default. However, it can easily be enabled:")
+    if st.button("Enable this feature for this session."):
+      st.session_state["REPLACEME_enabled"] = True
+      st.rerun()
+  pass
 if __name__ == "__main__": main()
