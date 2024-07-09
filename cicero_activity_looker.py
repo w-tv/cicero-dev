0f3e26dc94631aeac34afe705f1cf3e5d3990281
achangtv <104849382+achangtv@users.noreply.github.com>
2024-07-09 13:58:29 -0400

    change activity_looker from st.table to st.dataframe

--- cicero_activity_looker.py
+++ cicero_activity_looker.py
@@ -4,4 +4,16 @@ import streamlit as st
 from cicero_shared import sql_call_cacheless
 
 st.button("Refresh the page", help="Clicking this button will do nothing, but it will refresh the page, which is sometimes useful if this page loaded before the activity log was written to, and you want to see the new data in the activity log.")
-st.table(sql_call_cacheless("SELECT * FROM cicero.default.activity_log ORDER BY timestamp DESC LIMIT 20"))
+column_config = {1: "Timestamp",
+                 2: "User Email",
+                 3: "User Pod",
+                 4: "Prompter/Chatbot",
+                 5: "Prompt Sent",
+                 6: "Response Given",
+                 7: "Model Name",
+                 8: "Model URL",
+                 9: "Model Parameters",
+                 10: "System Prompt",
+                 11: "Base URL",
+                 12: "User Feedback"}
+st.dataframe(sql_call_cacheless("SELECT * FROM cicero.default.activity_log ORDER BY timestamp DESC LIMIT 20"), column_config=column_config)
