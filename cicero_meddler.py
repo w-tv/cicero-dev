ba315c050ca1d82f37595b73a3fe9c2c5e2130df
w-tv <wcarpenter@targetedvictory.com>
2024-07-18 20:59:29 -0700

    add account name adding to the meddler

--- cicero_meddler.py
+++ cicero_meddler.py
@@ -4,25 +4,17 @@ import streamlit as st
 from cicero_shared import sql_call_cacheless
 
 st.button("Refresh the page", help="Clicking this button will do nothing, but it will refresh the page, which is sometimes useful if this page loaded before the activity log was written to, and you want to see the new data in the activity log.")
-st.write("""TODO:
-
-  page that will let us:
-  
-  * add account(s) and rollup name(s)
+st.write("""TODO: page that will let us:
+  * add rollup name(s)
   * add a bio (corresponding to a rollup name)
   * add/remove client from the account list dropdown — and maybe some other stuff if you can think of anything"""
 )
-_ = """
-column_config = {1: "Timestamp",
-                 2: "User Email",
-                 3: "User Pod",
-                 4: "Prompter/Chatbot",
-                 5: "Prompt Sent",
-                 6: "Response Given",
-                 7: "Model Name",
-                 8: "Model URL",
-                 9: "Model Parameters",
-                 10: "System Prompt",
-                 11: "Base URL",
-                 12: "User Feedback"}
-st.dataframe(sql_call_cacheless("SELECT * FROM cicero.default.activity_log ORDER BY timestamp DESC LIMIT 20"), column_config=column_config)"""
+with st.expander("Account names"):
+  c = st.columns(2)
+  with c[0]:
+    acct = st.text_input("New account name").strip()
+  with c[1]:
+    #st.caption("")
+    if st.button("Add the account name to the list of account names in the database") and acct:
+      sql_call_cacheless("INSERT INTO cicero.default.client_list VALUES (:acct)", {"acct": acct}) # This is untested because I didn't want to mar the names. (:kongzi:)
+  st.table(sql_call_cacheless("SELECT * FROM cicero.default.client_list ORDER BY account_name ASC"))
