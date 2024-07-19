1ba59938f05f69243c16bb7db940cd0061c8b87c
w-tv <wcarpenter@targetedvictory.com>
2024-07-18 21:41:54 -0700

    INTERMEDIATE COMMIT: compare the current rollup to the rollup table, and find that the only difference is that in the former 'Mike Waltz - National Media': 'Michael Waltz', is changed to  'Mike Waltz - National Media': 'Mike Waltz', in the latter.

--- cicero_meddler.py
+++ cicero_meddler.py
@@ -2,6 +2,8 @@
 """ This shows you the top of the activity log, to make sure things are going through."""
 import streamlit as st
 from cicero_shared import sql_call_cacheless
+from cicero_topic_reporting import internal_account_name_to_external_account_name
+
 
 st.button("Refresh the page", help="Clicking this button will do nothing, but it will refresh the page, which is sometimes useful if this page loaded before the activity log was written to, and you want to see the new data in the activity log.")
 st.write("""TODO: page that will let us:
@@ -18,3 +20,10 @@ with st.expander("Account names"):
     if st.button("Add the account name to the list of account names in the database") and acct:
       sql_call_cacheless("INSERT INTO cicero.default.client_list VALUES (:acct)", {"acct": acct}) # This is untested because I didn't want to mar the names. (:kongzi:)
   st.table(sql_call_cacheless("SELECT * FROM cicero.default.client_list ORDER BY account_name ASC"))
+
+with st.expander("Rollup"):
+  c = st.columns(2)
+  with c[0]:
+    st.write(internal_account_name_to_external_account_name)
+  with c[1]:
+    st.write({k: v for k, v in sql_call_cacheless("SELECT Account_Name, Rollup_Name FROM cicero.ref_tables.ref_account_rollup SORT BY Account_Name ASC -- WHERE Visible_Frontend")}) #Last part doesn't work yet, so it's commented out.
