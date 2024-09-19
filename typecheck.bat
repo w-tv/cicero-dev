83fbe698135f9b57163dff5f8a6637cb61a196da
w-tv <wcarpenter@targetedvictory.com>
2024-09-19 00:20:48 -0700

    fix the fact that mypy wasn't looking at our google stubs

--- typecheck.bat
+++ typecheck.bat
@@ -1,8 +1,12 @@
 @echo off
 uv --version || pip install uv --disable-pip-version-check --break-system-packages
 uv run ruff check --select F --exclude cicero_empty_template.py .
+echo ---------- IN THE NORMAL OPERATION OF THIS SCRIPT, SOME HARMLESS ERRORS WILL OCCUR HERE
 set PYRIGHT_PYTHON_FORCE_VERSION=latest
 export PYRIGHT_PYTHON_FORCE_VERSION=latest # One of these two lines will work, depending on your operating system...
+python -c "from os import remove as r; r('.venv/Lib/site-packages/google/oauth2/py.typed') #We have to do these, or the stub library doesn't work in mypy, as it is overridden by the type signatures of the project itself I guess. (Later versions of the official library may have all of the types we need, in which case this logic will be removed from here. google-auth-library-python-BUG-WORKAROUND (in the sense that they don't provide types, but have a py.typed))"
+python -c "from os import remove as r; r('.venv/Lib/site-packages/google/auth/py.typed') #Can't do both in the same command because the first one might exit with an exception but the second one might still need to be done."
+echo ---------- END OF HARMLESS ERRORS
 uv run -- pyright --version || echo "Detected that pyright isn't installed. This implies the rest of the venv likely isn't ready yet either. Run the run.bat, which will install everything, and then try this again." && exit
 uv run pyright
 echo "-------- Mypy, non-interactively installing types..."
